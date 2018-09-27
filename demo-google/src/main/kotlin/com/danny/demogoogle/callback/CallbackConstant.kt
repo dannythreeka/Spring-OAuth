c9a10c18fcfff07f13a26e143da083e2969a3217
danny.wang <dannythreekai@gmail.com>
2018-09-27 17:30:19 +0900

    Complete the google callback logic.

--- demo-google/src/main/kotlin/com/danny/demogoogle/callback/CallbackConstant.kt
+++ demo-google/src/main/kotlin/com/danny/demogoogle/callback/CallbackConstant.kt
@@ -9,5 +9,10 @@ object CallbackConstant{
 
     object AUTHORIZATION{
         const val KEY_CODE = "code"
+        const val KEY_ERROR = "error"
+        const val KEY_GRANT_TYPE = "grant_type"
+        const val KEY_CLIENT_ID = "client_id"
+        const val KEY_CLIENT_SECRET = "client_secret"
+        const val KEY_REDIRECT_URI = "redirect_uri"
     }
 }
